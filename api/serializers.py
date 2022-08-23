e36562b21725126a1aa3ce244dae5f0c7c9bd5c5
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-08-23 18:06:09 +0430

    added serializers.py to api app and change timezone to for iran

---
+++ api/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from blog.models import Post
+from django.contrib.auth import get_user_model
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    def validate_title(self, value):
+        filter_list = ['java', 'javascript', 'asp.net']
+        for item in filter_list:
+            if item in value:
+                raise serializers.ValidationError(f'{item} really???!!!')
+        return value
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = '__all__'

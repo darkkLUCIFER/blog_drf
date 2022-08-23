5db6afc5453da63d0446628c80c422bd9948a639
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-08-23 19:08:18 +0430

    make views for Post and User and those urls with viewset

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import PostViewSet, UserViewSet
 
 app_name = 'api'
 
+router = routers.SimpleRouter()
+router.register('posts', PostViewSet, basename='post')
+router.register('users', UserViewSet, basename='users')
 urlpatterns = [
+    path('', include(router.urls)),
 
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from blog.models import Post
+from .serializers import PostSerializer, UserSerializer
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.filter(status=True)
+    serializer_class = PostSerializer
+
+
+class UserViewSet(ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer

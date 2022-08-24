c0316d624832e6e2455270bb3c0639a8041556e9
Mahdi <88978694+darkkLUCIFER@users.noreply.github.com>
2022-08-24 14:33:45 +0430

    added permission_classes to views

--- api/views.py
+++ api/views.py
@@ -3,13 +3,22 @@ from blog.models import Post
 from .serializers import PostSerializer, UserSerializer
 from django.contrib.auth import get_user_model
 from rest_framework.viewsets import ModelViewSet
+from .permissions import IsSuperUserOrStaffOrReadOnly, IsAuthorOrReadOnly, IsStaffOrReadOnly
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.filter(status=True)
     serializer_class = PostSerializer
 
+    def get_permissions(self):
+        if self.action == ['list', 'create']:
+            permission_classes = [IsStaffOrReadOnly]
+        else:
+            permission_classes = [IsStaffOrReadOnly, IsAuthorOrReadOnly]
+        return [permission() for permission in permission_classes]
+
 
 class UserViewSet(ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+    permission_classes = ['IsSuperUserOrStaffOrReadOnly']
